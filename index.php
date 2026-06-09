83c82c90e7fc8e70939c352479bf86b64eadfe94
Eric <erikbritejr@gmail.com>
2026-06-09 17:16:39 -0300

    Limpiar repositorio

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-// ╔══════════════════════════════════════════════════════════╗
-// ║  BRITECH — index.php (raíz del proyecto)                ║
-// ║  Redirige automáticamente a la tienda pública            ║
-// ╚══════════════════════════════════════════════════════════╝
-header('Location: views/tienda.html');
-exit;
